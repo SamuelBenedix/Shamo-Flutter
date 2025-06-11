ff752d56cc86cf42c1f748c1140cb015ab6c3a4b
Samuel <68117536+SamuelBenedix@users.noreply.github.com>
2025-06-11 16:10:11 +0700

    initialApp

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello, Flutter!',
+          style: GoogleFonts.poppins(fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
