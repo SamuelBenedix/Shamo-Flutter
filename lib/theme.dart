027383589287726868c9cdf6f789f101d44c41d8
Samuel <68117536+SamuelBenedix@users.noreply.github.com>
2025-06-11 16:32:25 +0700

    theme dart

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xFF6C5ECF);
+Color secondaryColor = const Color(0x38ABBE);
+Color alertColor = const Color(0xED6383);
+Color priceColor = const Color(0x2C96F1);
+Color backgroundColor1 = const Color(0x1F1D28);
+Color backgroundColor2 = const Color(0x2B2937);
+Color backgroundColor3 = const Color(0x242231);
+Color primaryTextColor = const Color(0xE1E1E1);
+Color secondaryTextColor = const Color(0x999999);
